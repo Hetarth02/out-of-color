95b0e2eacb606f3632dbb8043f3793681b96ec85
Sanjay Boricha <sanjayboricha1001@gmail.com>
2025-11-15 10:21:52 +0530

    Implement basic night mode and fix turret placement

--- scenes/maps/components/turret_placement.gd
+++ scenes/maps/components/turret_placement.gd
@@ -1,6 +1,11 @@
 extends TextureButton
 
+@onready var point_light_2d: PointLight2D = $PointLight2D
+
 var turret = preload("res://scenes/weapons/tank.tscn")
+
+func _ready() -> void:
+	point_light_2d.visible = Global.is_night
 
 func _on_pressed() -> void:
 	var new_turret: Node2D = turret.instantiate()
--- scripts/global.gd
+++ scripts/global.gd
@@ -8,16 +8,13 @@ enum EnemyTypes {
 
 signal wave_start(wave)
 
-var selected_map := ""
-var turretsNode : Node2D
-var projectilesNode : Node2D
-var currentMap : Node2D
-
+var is_night: bool = false
 
 var hud: Control
 var wave_count: Label = null
 var main_node: Node2D
-var map_node: Node2D
+var current_map: Node2D
+var selected_map: Node2D
 
 func _ready() -> void:
 	wave_start.connect(on_wave_start)
@@ -26,8 +23,8 @@ func on_wave_start(wave) -> void:
 	wave_count.text = "Wave: " + str(wave)
 
 func restart_current_level():
-	var currentLevelScene := load(currentMap.scene_file_path)
-	currentMap.queue_free()
+	var currentLevelScene := load(current_map.scene_file_path)
+	current_map.queue_free()
 	var new_map = currentLevelScene.instantiate()
 	new_map.map_type = selected_map
 	main_node.add_child(new_map)
